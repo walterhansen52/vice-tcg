b1f36b2a07cf5ac1dbb642cda96a7aa23aa427d7
walterhansen52 <walterhansen52@gmail.com>
2025-07-08 12:28:41 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,68 +1,62 @@
 
-let hand = [];
-let deck = [];
 let turn = 1;
-let vice = 1;
+let maxVice = 1;
+let currentVice = 1;
+let log = document.getElementById("log");
+let hand = [];
+let handArea = document.getElementById("hand");
+let playerHP = 25;
+let aiHP = 25;
 
-function log(message) {
-    const logDiv = document.getElementById("log");
-    logDiv.innerHTML += message + "<br>";
-    logDiv.scrollTop = logDiv.scrollHeight;
+function logMessage(msg) {
+  log.value += msg + "\n";
+  log.scrollTop = log.scrollHeight;
 }
 
 function drawCard() {
-    if (deck.length > 0) {
-        const card = deck.pop();
-        hand.push(card);
-        log("Drew card: " + card.name);
-    } else {
-        log("Deck is empty.");
-    }
+  let cardId = Math.floor(Math.random() * 100);
+  let card = { id: cardId, cost: 1 };
+  hand.push(card);
+  updateHand();
+  logMessage("Drew card: Card " + cardId);
 }
 
-function startGame() {
-    deck = [];
-    for (let i = 1; i <= 20; i++) {
-        deck.push({ name: "Card " + i, cost: 1 });
-    }
-    for (let i = 0; i < 5; i++) {
-        drawCard();
-    }
-    renderHand();
-    document.getElementById("game-status").textContent = "Game started. Your turn.";
-    log("Game started. Drew starting hand of 5 cards.");
-}
-
-function renderHand() {
-    const handDiv = document.getElementById("hand");
-    handDiv.innerHTML = '';
-    hand.forEach((card, index) => {
-        const div = document.createElement("div");
-        div.className = "card";
-        div.textContent = card.name + " (Cost: " + card.cost + ")";
-        div.onclick = () => playCard(index);
-        handDiv.appendChild(div);
-    });
+function updateHand() {
+  handArea.innerHTML = "";
+  hand.forEach((card, index) => {
+    let btn = document.createElement("button");
+    btn.innerText = "Card " + card.id + " (Cost: " + card.cost + ")";
+    btn.onclick = () => playCard(index);
+    handArea.appendChild(btn);
+  });
 }
 
 function playCard(index) {
-    const card = hand[index];
-    if (card.cost <= vice) {
-        log("Played " + card.name + ", Cost: " + card.cost);
-        vice -= card.cost;
-        hand.splice(index, 1);
-        renderHand();
-    } else {
-        log("Not enough Vice to play " + card.name);
-    }
+  let card = hand[index];
+  if (card.cost <= currentVice) {
+    currentVice -= card.cost;
+    document.getElementById("vice-counter").innerText = "Vice: " + currentVice;
+    hand.splice(index, 1);
+    updateHand();
+    logMessage("Played Card " + card.id + ", Cost: " + card.cost);
+  } else {
+    logMessage("Not enough Vice to play Card " + card.id);
+  }
 }
 
-function endTurn() {
-    turn++;
-    vice = turn;
-    log("Turn " + turn + " started. Vice replenished to " + vice);
-    drawCard();
-    renderHand();
+document.getElementById("end-turn").onclick = () => {
+  turn++;
+  maxVice++;
+  currentVice = maxVice;
+  document.getElementById("vice-counter").innerText = "Vice: " + currentVice;
+  drawCard();
+  logMessage("Turn " + turn + " started. Vice replenished to " + maxVice);
+};
+
+function startGame() {
+  logMessage("Game started. Drawing starting hand of 5 cards.");
+  for (let i = 0; i < 5; i++) drawCard();
+  document.getElementById("status").innerText = "Game started. Your turn.";
 }
 
-window.onload = startGame;
+startGame();
